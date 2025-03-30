eedf1f5ed1de44f9705be5026e9a5e264ded645d
mr-dj123 <142316757+mr-dj123@users.noreply.github.com>
2025-03-31 06:01:51 +0800

    Add app/src/main/java/com/phinma/utils/Extensions.kt 1743372110734

---
+++ app/src/main/java/com/phinma/utils/Extensions.kt
@@ -0,0 +1,16 @@
+package com.phinma.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun String.isValidEmail(): Boolean {
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
+
+fun String.isValidPassword(): Boolean {
+    return this.length >= 6
+}
